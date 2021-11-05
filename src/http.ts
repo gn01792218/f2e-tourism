2dcc3907629726dacdad731cf937e68a5620b8e9
MSI\gn01792218 <gn01792218@hotmail.com>
2021-11-05 21:37:52 +0800

    //API:成功於請求時代入會員Token，獲得每日2萬次的請求次數

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
-
+import jsSHA from 'jssha';
 
 // axios實例
 const service = axios.create({
@@ -7,6 +7,9 @@ const service = axios.create({
   headers: { 'Content-Type': 'application/json' },
   timeout: 20000 // 超时时间
 });
+
+//
+const header = getAuthorizationHeader() as AxiosRequestConfig
 // 请求攔截
 service.interceptors.request.use(
   (config: AxiosRequestConfig) => {
@@ -52,11 +55,25 @@ service.interceptors.response.use(
     return Promise.reject(errMsg);
   }
 );
+//加密herder
+function getAuthorizationHeader() {
+  //  填入自己 ID、KEY 開始
+      let AppID = '0369de6f30be4e2ea30386c8c3c3ef6b';
+      let AppKey = 'cf-8qbLmfnA7Hv7KBZAcoVlpVSM';
+  //  填入自己 ID、KEY 結束
+      let GMTString = new Date().toUTCString();
+      let ShaObj = new jsSHA('SHA-1', 'TEXT');
+      ShaObj.setHMACKey(AppKey, 'TEXT');
+      ShaObj.update('x-date: ' + GMTString);
+      let HMAC = ShaObj.getHMAC('B64');
+      let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+      return {headers:{ 'Authorization': Authorization, 'X-Date': GMTString }}; 
+  }
 export default (method:string ,url:string ,data = null , config?:any)=>{
     method = method.toLowerCase()
     switch(method){
         case 'get':
-            return service.get(url)
+            return service.get(url,header)
     }
 
 }
--- src/store/Activity.ts
+++ src/store/Activity.ts
@@ -25,7 +25,6 @@ export const state = {
     }
   };
   export const getters = {
-   
   }
   
   export default {
